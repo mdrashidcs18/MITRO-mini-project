51cd6700d1e45ed812ea2877627e307ca13bf218
Anubhavgupta112000 <anubhav.gupta_cs18@gla.ac.in>
2021-04-20 19:44:38 +0530

    final commit

---
+++ InstaClone/config/__test__/test.config.js
@@ -0,0 +1,10 @@
+// IF any of the ID below is not present in the database, then replace it with a valid ID.
+
+module.exports = {
+  user1: 24,
+  user2: 7,
+  postID: 89, 
+  conId: 24, 
+  grpID: 11, 
+  testMailTo: 'anymail@gmail.com',
+}
